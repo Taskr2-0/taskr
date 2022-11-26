124724d35d0fc8f3607304b6ff8766a7a1b49eb8
Ona Narbutas <ona.narbutas@gmail.com>
2022-11-26 09:43:07 -0500

    Add routes for server to handle requests to create/read/update/delete tickets

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -22,13 +22,34 @@ router.post('/login', userController.login, (req, res) => {
 })
 
 // route requests to get user tickets
+router.get('/usertickets', ticketController.getUserTickets, (req, res) => {
+  console.log ('exited get user tickets middleware, preparing get tickets reesponse');
+  return res.status(200).json(res.locals.userTickets); // expecting ticketController.getUserTickets to save db response as locals property userTickets
+})
 
-// route requests to get admin tickets
 
 // route requests for user to create a new ticket
+router.post('/usertickets', ticketController.createTicket, (req, res) => {
+  console.log ('exited createTicket middleware, preparing response');
+  return res.status(200).json(res.locals.newTicket); // expecting ticketController.createTicket to save db resposne to res locals property newTicket
+})
 
 // route requests for user to delete ticket
+router.delete('/usertickets', ticketController.deleteTicket, (req, res) => {
+  console.log ('exited deleteTicket middleware, preparing response');
+  return res.status(200).json(res.locals.deletedTicket); // expecting ticketController.deleteTicket to save db response to locals as deletedTicket
+})
+
+// route requests to get admin tickets
+router.get('/admintickets', ticketController.getAdminTickets, (req, res) => {
+  console.log ('exited get admin tickets middleware, preparing get tickets reesponse');
+  return res.status(200).json(res.locals.adminTickets); // expecting ticketController.getAdminTickets to save db response as locals property adminTickets
+})
 
 // route requests for admin to update ticket status
+router.patch('/admintickets', ticketController.updateTicket, (req, res) => {
+  console.log ('exited updateTicket middleware, preparing response');
+  return res.status(200).json(res.locals.updatedTicket); // expecting ticketController.updateTicket to save db response to locals as updatedTicket
+})
 
 module.exports = router;
