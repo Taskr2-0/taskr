288eab961575b22b4473638eaec7a298fcd0f4bd
Jo Huang <johuang@Jos-MacBook-Air.local>
2022-11-27 13:19:39 -0500

    added a fetch call to send POST request to the /api/admintickets end point and rendered tickets on dashboard

--- client/views/Dashboard.js
+++ client/views/Dashboard.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Ticket from "./Ticket";
 
 const Dashboard = () => {
+    const [ taskArr, setTaskArray ] = useState(() => []);
+    useEffect(() => {
+        fetch('/api/admintickets')
+            .then(res => res.json())
+            .then(data => {
+                setTaskArray(() => data)
+            })
+    },[])
+    console.log('this is task outside', taskArr)
+    
+    const tickets = [];
+    for (let i = 0; i < taskArr.length; i++) {
+        tickets.push(<Ticket taskId={taskArr[i].id} 
+                             userId={taskArr[i].user_id}
+                             taskTitle={taskArr[i].title}
+                             taskDesc={taskArr[i].description}
+                             taskStatus={taskArr[i].status}
+                             taskPriority={taskArr[i].priority}   
+                    />)
+    }
+    console.log(tickets)
 
     return (
         <div className = "dashboard">
             <h1>Dashboard</h1>
-            <Ticket />
+            {tickets}
         </div>
     )
 
