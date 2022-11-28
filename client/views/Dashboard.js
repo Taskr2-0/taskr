134abf352e61c2ff3a5403ed8e15e2720f52b022
Zhaowei <sunzhaow0813@gmail.com>
2022-11-28 11:05:03 -0500

    completed admin update status functionality

--- client/views/Dashboard.js
+++ client/views/Dashboard.js
@@ -1,49 +1,46 @@
 import React, { useEffect, useState } from "react";
 import Ticket from "./Ticket";
-
 const Dashboard = (props) => {
     const [ taskArr, setTaskArray ] = useState(() => []);
-
     // if is_admin is true, make fetch request to admintickets end point
-    // if is_admin is false, make fetch request to usertickets end point with id in header
-    const route = (props.userDetails.is_admin) ? '/api/admintickets' : '/api/usertickets';
-    console.log('props in dashboard: ', props);
-
-    useEffect(() => {
-        fetch(route, {
-            method: 'GET',
-            headers: {
-                'id': props.userDetails.id
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            setTaskArray(() => data)
-        })
-    },[])
-
+    // if (props.userDetails.is_admin === true) {
+        useEffect(() => {
+            fetch('/api/admintickets')
+                .then(res => res.json())
+                .then(data => {
+                    setTaskArray(() => data)
+                })
+        },[])
+    // }
     console.log('this is task outside', taskArr)
-        
-    
+    // if is_admin is false, make fetch request to usertickets end point with id in header
+    function renderPageAfterUpdate() {
+        fetch('/api/admintickets')
+                .then(res => res.json())
+                .then(data => {
+                    setTaskArray(() => data)
+        })
+    }
+    const newTaskArr = taskArr.sort((a, b) => b.priority - a.priority);
     const tickets = [];
-    for (let i = 0; i < taskArr.length; i++) {
-        tickets.push(<Ticket taskId={taskArr[i].id} 
+    for (let i = 0; i < newTaskArr.length; i++) {
+        tickets.push(<Ticket taskId={taskArr[i].id}
                              userId={taskArr[i].user_id}
                              taskTitle={taskArr[i].title}
                              taskDesc={taskArr[i].description}
                              taskStatus={taskArr[i].status}
-                             taskPriority={taskArr[i].priority}   
+                             taskPriority={taskArr[i].priority}
+                             isAdmin={props.userDetails.is_admin}
+                             renderPageAfterUpdate={renderPageAfterUpdate}
+                             userDetails={props.userDetails}
                     />)
     }
     console.log(tickets)
-
     return (
         <div className = "dashboard">
             <h1>Dashboard</h1>
             {tickets}
         </div>
     )
-
 };
-
 export default Dashboard;
