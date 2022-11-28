0377c5cf40ae66c3444768b02b3b93a20f181a1b
Jo Huang <johuang@Jos-MacBook-Air.local>
2022-11-28 10:34:23 -0500

    adding new request button on main page

--- client/views/MainPage.js
+++ client/views/MainPage.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React , {useState} from "react";
 import TicketCreator from "./TicketCreator";
 import Header from "./Header";
 import Dashboard from "./Dashboard";
 
 
 const MainPage = (props) => {
+    const [showComponent, setShowComponent] = useState(false);
+    // console.log('this is show component', showComponent)
 
     return (
         <div className="mainPage">
             <Header userDetails = {props.userDetails}/>
             <Dashboard userDetails = {props.userDetails}/>
-            <TicketCreator userDetails = {props.userDetails}/>
+            <button className="newRequest" onClick={() => setShowComponent(true)}>New Request</button>
+            {/* <TicketCreator userDetails = {props.userDetails}/> */}
+            {showComponent && <TicketCreator userDetails = {props.userDetails}/>}
+
         </div>
     )
 
