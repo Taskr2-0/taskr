2dc4bc2c5386a73ecf531524cebe6ccbe816062b
dison  ruan <disonruan@disons-mbp.myfiosgateway.com>
2022-12-01 10:59:42 -0500

    fixed session issue with logout

--- client/views/App.js
+++ client/views/App.js
@@ -4,60 +4,60 @@ import MainPage from "./MainPage";
 
 // will render either landing page or main page based on if user is logged in
 const App = (props) => {
-    const userDefault = {
+  const userDefault = {
+    id: null,
+    email: null,
+    first_name: null,
+    last_name: null,
+    is_admin: null,
+  };
+
+  const [isLoggedIn, setIsLoggedIn] = useState(() => false);
+  const [userDetails, setUserDetails] = useState();
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((user) => {
+        setUserDetails(user);
+        setIsLoggedIn(true);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      setUserDetails({
         id: null,
         email: null,
         first_name: null,
         last_name: null,
-        is_admin: null
-    };
-
-    const [isLoggedIn, setIsLoggedIn] = useState(() => false);
-    const [userDetails, setUserDetails] = useState();
-
-    useEffect(() => {
-        fetch('/api')
-        .then(res => res.json())
-        .then(user => {
-            setUserDetails(user);
-            setIsLoggedIn(true);
-        })
-    }, []);
-
-    useEffect(() => {
-        if(!isLoggedIn){
-            fetch('/api/logout')
-            setUserDetails({
-                id: null,
-                email: null,
-                first_name: null,
-                last_name: null,
-                is_admin: null
-            })
-        }
-    }, [isLoggedIn]);
-
-    function logIn() {
-        setIsLoggedIn(() => true);
-    };
-
-    function logOut(){
-        setIsLoggedIn(() => false);
+        is_admin: null,
+      });
     }
-    
-    function updateUser(userObject) {
-        setUserDetails(userObject);
-    };
-    
-    return (
-        <div className="app">
-            {isLoggedIn 
-            ? <MainPage userDetails = {userDetails} logOut={logOut}/>
-            : <LandingPage logIn = {logIn} updateUser = {updateUser}/>
-            }
+  }, [isLoggedIn]);
 
-        </div>
-    )
-}
+  function logIn() {
+    setIsLoggedIn(() => true);
+  }
+
+  function logOut() {
+    fetch("/api/logout");
+    setIsLoggedIn(() => false);
+  }
+
+  function updateUser(userObject) {
+    setUserDetails(userObject);
+  }
+
+  return (
+    <div className="app">
+      {isLoggedIn ? (
+        <MainPage userDetails={userDetails} logOut={logOut} />
+      ) : (
+        <LandingPage logIn={logIn} updateUser={updateUser} />
+      )}
+    </div>
+  );
+};
 
 export default App;
